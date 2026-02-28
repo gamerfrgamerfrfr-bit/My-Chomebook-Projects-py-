a81a258d96570043bcd9ee11531c96326e9a5af7
Bob <263774652+gamerfrgamerfrfr-bit@users.noreply.github.com>
2026-02-28 04:05:44 +0000

    Added Colors!

--- Number_game.py
+++ Number_game.py
@@ -1,9 +1,22 @@
 import random
+# ... Colors to use! ... #
+RED = "\033[91m"
+BLUE = "\033[R94m"
+END = "\033[0m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+MAGENTA = "\033[95m"
+CYAN = "\033[96m"
+WHITE = "\033[97m"
+BOLD = "\033[1m"
+END = "\033[0m"
 
+# ... Choose's the number ... #
 secret_number = random.randint(1,10)
-guess = int(input("guess a number between 1 and 10:"))
+
+guess = int(input(f"{BOLD}{WHITE} Guess a number between 1 and 10:{END} "))
 
 if guess == secret_number:
-    print("You got the number!")
+    print(f"{GREEN} You got the number!{END} ")
 else:
-    print(f"Nope! It was {secret_number}")
+    print(f"{RED} Nope!{END}{YELLOW} It was {secret_number}!{END}")
